f2bd30ce28254a75d378bb9ba47847fcaf00da9a
Kirill Peshin <kirill_peshin@epam.com>
2022-03-29 08:53:50 +0300

    Upd

--- app/src/test/java/com/example/javaandroid/CalcSecondTest.java
+++ app/src/test/java/com/example/javaandroid/CalcSecondTest.java
@@ -14,25 +14,25 @@ public class CalcSecondTest {
     private Calc calc;
 
     @Test
-    public void calc_add_isCorrect() {
+    public void calc_second_add_isCorrect() {
         calc = new Calc();
         assertEquals(5.0, calc.add(2.5, 2.5), 0.1);
     }
 
     @Test
-    public void calc_subtract_isCorrect() {
+    public void calc_second_subtract_isCorrect() {
         calc = new Calc();
         assertEquals(1.0, calc.subtract(3.3, 2.3), 0.1);
     }
 
     @Test
-    public void calc_multiply_isCorrect() {
+    public void calc_second_multiply_isCorrect() {
         calc = new Calc();
         assertEquals(10.0, calc.multiply(100.0, 0.1), 0.1);
     }
 
     @Test
-    public void calc_divide_isCorrect() {
+    public void calc_second_divide_isCorrect() {
         calc = new Calc();
         assertEquals(10.0, calc.divide(100.0, 10.0), 0.1);
         assertEquals(0.0, calc.divide(10.0, 0.0), 0.1);
